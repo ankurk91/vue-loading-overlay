b32156260bc152c48b6f66a915248848934734bd
ankurk91 <pro.ankurk1@gmail.com>
2018-08-13 09:42:46 +0530

    Fix: window object detection in util itself

--- src/js/util.js
+++ src/js/util.js
@@ -10,6 +10,6 @@ const hasWindow = () => {
   return typeof window !== 'undefined';
 };
 
-const HTMLElement = !hasWindow ? Object : window.HTMLElement;
+const HTMLElement = hasWindow() ? window.HTMLElement : Object;
 
 export {removeElement, hasWindow, HTMLElement}
