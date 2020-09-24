8b8254207efe6555faae25c095fab57da09456d0
Ankur Kumar <ankurk91@users.noreply.github.com>
2020-09-24 08:12:19 +0530

    Fix broken docs
    Docs: Restore good old code

--- examples/index.js
+++ examples/index.js
@@ -2,11 +2,20 @@
 
 import Vue from 'vue';
 
+Vue.config.productionTip = false;
+
 // Bootstrap is being used for demonstration only
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.vue';
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).mount('#app');
+  created() {
+    console.log('Main app created');
+  },
+  mounted() {
+    console.log('Main app mounted');
+  },
+});
