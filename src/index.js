218108aba0002fc7168229e06de7fd4599d38544
ankurk91 <pro.ankurk1@gmail.com>
2021-05-09 08:56:45 +0530

    Fix: plugin usage,#81

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Component from './js/Component.vue';
 import './css/index.css';
 
 const Plugin = (app, props = {}, slots = {}) => {
-  const instance = useLoading(app, props, slots);
+  const instance = useLoading(props, slots);
   app.config.globalProperties.$loading = instance;
   app.provide('$loading', instance)
 };
