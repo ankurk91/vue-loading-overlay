296bc936d5d4971db704a234c1e64367ed60b5cd
ankurk91 <pro.ankurk1@gmail.com>
2021-06-13 10:44:57 +0530

    Update deps

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  testURL: 'http://localhost',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'node',
+    'vue',
+    'css'
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': '<rootDir>/__test__/__mocks__/styleMock.js'
+  },
+}
